c9266a3687e725ccbb4a19ae880f0460a4852089
Alyssa G <agregorashb@gmail.com>
2025-05-19 11:48:04 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "graduationcat.ca",
+	site: "www.graduationcat.ca",
 	integrations: [
 		tailwind(),
 		alpinejs(),
